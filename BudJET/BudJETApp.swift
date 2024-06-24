24034716ccfb852f60296731f72778fd82a41cf2
Anushri Agrawal <anushrisugarland@gmail.com>
2024-06-24 13:43:30 -0500

    Initial Commit

---
+++ BudJET/BudJETApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudJETApp.swift
+//  BudJET
+//
+//  Created by Scholar on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BudJETApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
